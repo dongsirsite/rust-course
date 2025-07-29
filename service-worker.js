49060188d1492df052d54d4db945ae36c1b7cac1
dongsirsite <dongsirsite@users.noreply.github.com>
2025-07-29 15:05:30 +0000

    deploy: 4badd324514f793ecf7e0d3791f00366ba23356f

--- service-worker.js
+++ service-worker.js
@@ -5,8 +5,8 @@ const CACHE_ASSETS = [
   '/style.css',
   '/app.js',
   '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', e => {
